27637711d0eaffc5685978fd68bf4418b6b7cfcc
Mike North <michael.l.north@gmail.com>
2016-02-17 02:07:33 +0800

    Do not pass params to socket.connect

--- tests/unit/phoenix-test.js
+++ tests/unit/phoenix-test.js
@@ -5,20 +5,20 @@ module('phoenix javascript module');
 
 
 test('it works', function(assert) {
-  assert.expect(3);
+  assert.expect(2);
   let done = assert.async();
   let socket = new Socket('/socket', {
     logger: ((kind, msg, data) => {
       console.log(`${kind}: ${msg}`, data);
-    })
+    }),
+    params: {user_id: "123"}
   });
   assert.ok(socket);
 
-  socket.connect({user_id: "123"});
-  socket.onError(() => {
-    assert.ok(true, 'Did Error');
-  });
+  socket.connect();
+
   socket.onClose(() => {
+    console.log('close');
     assert.ok(true, 'Did Close');
     done();
   });
