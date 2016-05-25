5398d3d5ea58561b114f057a9b0ac279a5af9941
Mike North <michael.l.north@gmail.com>
2016-05-25 11:55:20 +0800

    Fastboot compatibility

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
 'use strict';
+var esTranspiler = require('broccoli-babel-transpiler');
+var BroccoliMergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
-  name: 'phoenix'
+  name: 'phoenix',
+
+  included: function(app) {
+    this._super.included(app);
+    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
+      app.import('vendor/phoenix.js');
+    }
+  }
 };
