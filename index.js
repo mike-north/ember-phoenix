1ce8101148069fd7c417714da2094f66bec89b3d
Brian Cardarella <brian@dockyard.com>
2017-05-22 08:59:50 -0400

    Use Phoenix node module

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 /* eslint-env node */
 'use strict';
 
+const Path = require('path');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+
+function isNotFastBoot() {
+  return typeof FastBoot === 'undefined';
+}
+
 module.exports = {
   name: 'phoenix',
 
-  included: function(app) {
-    this._super.included(app);
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      app.import('vendor/phoenix.js');
-    } else {
-      app.import('vendor/phoenix-stub.js');
+  treeForAddon(tree) {
+    if (isNotFastBoot()) {
+      tree = this.addPhoenixToTree(tree);
     }
+
+    return this._super.treeForAddon.call(this, tree);
+  },
+
+  addPhoenixToTree(tree) {
+    let phoenixPath = Path.join(this.project.nodeModulesPath, 'phoenix');
+
+    let phoenixTree = new Funnel(phoenixPath, {
+      files: ['web/static/js/phoenix.js'],
+      getDestinationPath(relativePath) {
+        if (relativePath.indexOf('phoenix.js') > -1) {
+          return 'index.js';
+        }
+      }
+    });
+
+    return new MergeTrees([tree, phoenixTree]);
   }
 };
